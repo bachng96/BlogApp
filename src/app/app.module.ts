d386eeee332d9cde0cb80e8fd313b177215deaf8
Nguyen Gia Bach <bachng96@gmail.com>
2022-07-15 09:47:18 +0700

    create component and add bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './components/page/home/home.component';
+import { LoginComponent } from './components/page/login/login.component';
+import { SignupComponent } from './components/page/signup/signup.component';
+import { SettingComponent } from './components/page/setting/setting.component';
+import { EditorComponent } from './components/page/editor/editor.component';
+import { ArticleComponent } from './components/page/article/article.component';
+import { ProfileComponent } from './components/page/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    SettingComponent,
+    EditorComponent,
+    ArticleComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
